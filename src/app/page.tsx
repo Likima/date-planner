bd8caa3589ae4985f1c50efff8d6dbfb224f5b07
Brandon's Zenbook <tangbrandonk@gmail.com>
2025-05-14 13:57:30 -0700

    Map shows up on screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 import { Header } from "@/src/components/header";
+import Map from "@/src/components/map";
+
+import "mapbox-gl/dist/mapbox-gl.css";
+import mapboxgl from "mapbox-gl"
 
 export default function Home() {
   return(
@@ -7,7 +11,8 @@ export default function Home() {
       <div className = "bg-indigo-400 p-5 fixed inset-x-0 top-0 z-10 border-b border-black/5">
         <Header />
       </div>
-      <div className = "h-screen flex items-center justify-center text-bold text-5xl">
+      <Map />
+      <div className = "h-screen flex items-center justify-center text-bold text-5xl z-10">
         Welcome to Date Planner!  
       </div>
     </div>
---
+++ src/components/map.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import mapboxgl from "mapbox-gl";
+
+const Map = () => {
+  const mapContainer = useRef(null);
+  const mapRef = useRef<mapboxgl.Map | null>(null);
+
+  useEffect(() => {
+    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_KEY || "";
+
+    if (mapContainer.current) {
+      mapRef.current = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: "mapbox://styles/mapbox/dark-v11",
+        center: [-123.12, 49.28],
+        zoom: 9,
+        attributionControl: false, // Disable the footer
+      });
+    }
+  }, []);
+
+  return <div ref={mapContainer} style={{ width: "100%", height: "100vh" }} />;
+};
+
+export default Map;
