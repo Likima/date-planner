76aa989a8f63c770e29730a1a963a36c0170dbe8
Brandon-Desktop <tangbrandonk@gmail.com>
2025-05-18 18:12:27 -0700

    geolocate user on launch

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 export function Header(props: React.PropsWithChildren) {
   return (
-    <div className="absolute bg-indigo-400 p-5 inset-x-2 top-0 z-10 border-b border-black/5">
+    <div className="absolute p-5 inset-x-2 top-0 z-100 border-b backdrop-blur-sm bg-indigo-400/10 m-5 rounded-2xl">
       <header className="text-blue-300 hover-text-blue-600 font-mono text-xl text-bold">
         <div className="flex bg-green justify-around">
           <Link href="/Home"> Home </Link>
--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,11 +1,77 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import mapboxgl from "mapbox-gl";
 
+interface Coordinates {
+  latitude: number;
+  longitude: number;
+  accuracy: number;
+}
+
+interface GeolocationPosition {
+  coords: Coordinates;
+}
+
+interface GeolocationError {
+  code: number;
+  message: string;
+}
+
+function errors(err: GeolocationError): void {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
 const Map = () => {
+
+  function success(pos: GeolocationPosition): void {
+    var crd = pos.coords;
+    console.log("Your current position is:");
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+  }
+
+  var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+
+  const [location, setLocation] = useState(null);
   const mapContainer = useRef(null);
   const mapRef = useRef<mapboxgl.Map | null>(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.permissions
+        .query({ name: "geolocation" })
+        .then(function (result) {
+          console.log(result);
+        })
+    } else {
+      console.log("Geolocation not supported");
+    }
+  }, [])
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.permissions
+        .query({ name: "geolocation" })
+        .then(function (result) {
+          console.log(result);
+          if (result.state === "granted") {
+            navigator.geolocation.getCurrentPosition(success, errors, options);
+          } else if (result.state === "prompt") {
+            navigator.geolocation.getCurrentPosition(success, errors, options);
+          } else if (result.state === "denied") {
+            // implement denied later
+          }
+        });
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }, []);
 
   useEffect(() => {
     mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_KEY || "";
@@ -21,7 +87,7 @@ const Map = () => {
     }
   }, []);
 
-  return <div ref={mapContainer} style={{ width: "100%", height: "100vh" }} className = "z-10" />;
+  return <div ref={mapContainer} style={{ width: "100%", height: "100vh" }} className="z-10" />;
 };
 
 export default Map;
